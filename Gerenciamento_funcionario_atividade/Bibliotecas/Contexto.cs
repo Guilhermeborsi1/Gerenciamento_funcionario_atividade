d5f6d07aaf8a126f93725d30941433b5d1431632
GUILHERME DE SOUZA BORSI <guilherme.sborsi@senacsp.edu.br>
2022-09-19 21:01:08 -0300

    Adicionar arquivos de projeto.

---
+++ Gerenciamento_funcionario_atividade/Bibliotecas/Contexto.cs
@@ -0,0 +1,27 @@
+﻿using GerenciamentoFuncionario.Comuns.Modelos;
+using System.Collections.Generic;
+
+namespace GerenciamentoFuncionario.AcessoDados
+{
+    public class Contexto
+    {
+        public Contexto()
+        {
+            //Funcionarios = new List<Funcionario> {
+            //    new Funcionario("Fulano de Tal", 1, false),
+            //    new Funcionario("Ciclano de Tal", 2, true),
+            //    new Funcionario("Beltrano de Tal", 3, true)
+            //};
+
+            Cargos = new List<Cargo> {
+                new Cargo(1, "Desenvolvedor"),
+                new Cargo(2, "Engenheiro"),
+                new Cargo(3, "Arquiteto"),
+                new Cargo(4, "Gerente de Projetos")
+            };
+        }
+
+        public List<Cargo> Cargos { get; set; } = new List<Cargo>();
+        public List<Funcionario> Funcionarios { get; set; } = new List<Funcionario>();
+    }
+}
